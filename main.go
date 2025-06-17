c634edad66f5955158a892dfc07f4adc726089f4
Алексей Захаров <127926741+Frown1ngg@users.noreply.github.com>
2025-06-17 09:29:57 +0700

    Update main.go

--- main.go
+++ main.go
@@ -9,11 +9,4 @@ func init() {
 }
 func main() {
 	server.StartServer()
-	// router := gin.Default()
-	// router.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"message": "lol",
-	// 	})
-	// })
-	// router.Run(":8080")
 }
