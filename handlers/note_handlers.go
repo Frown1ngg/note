c9ca167a18bbbf7f9c05737988f1b9264c479835
Алексей Захаров <127926741+Frown1ngg@users.noreply.github.com>
2025-07-01 18:05:39 +0700

    upd handlers getnote-(s)

--- handlers/note_handlers.go
+++ handlers/note_handlers.go
@@ -1,26 +1,120 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"notes_project/database"
 	"notes_project/models"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 func GetNoteHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "GetNoteHandler")
+	//ctx.JSON(http.StatusOK, "GetNoteHandler")
+	authorId := 1
+	id := ctx.Param("id")
+
+	collection := database.MongoClient.Database("admin").Collection(fmt.Sprintf("notes/%d", authorId))
+
+	var note models.Note
+	filter := bson.M{"id": id}
+
+	errFind := collection.FindOne(ctx, filter).Decode(&note)
+	if errFind != nil {
+		ctx.JSON(http.StatusNotFound, "Заметка не найдена")
+	}
+	ctx.JSON(http.StatusOK, &note)
+
 }
 
 func GetNotesHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "GetNotesHandler")
+	// ctx.JSON(http.StatusOK, "GetNotesHandler")
+	authorId := 1
+	var notes []models.Note
+
+	val, err := database.RedisClient.Get(fmt.Sprintf("notes/%d", authorId)).Result()
+	if err == redis.Nil {
+		log.Printf("Кеш не найден, загружаем из БД")
+		// Получаем коллекцию "notes"
+		collection := database.MongoClient.Database("admin").Collection(fmt.Sprintf("notes/%d", authorId))
+
+		// Поиск документов без фильтров для получения всех заметок
+		cursor, err := collection.Find(ctx, bson.M{})
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		// Закрытие курсора, при завершении работы функции
+		defer cursor.Close(ctx)
+		// Итерация по курсору и декодирование документов в заметки
+		for cursor.Next(ctx) {
+			var note models.Note
+			err := cursor.Decode(&note)
+			if err != nil {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+			notes = append(notes, note)
+		}
+		// Проверка на ошибки после итерации
+		if err := cursor.Err(); err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		// Проверка на наличие заметок
+		if len(notes) == 0 {
+			ctx.JSON(http.StatusOK, "Заметок не найдено")
+		} else {
+			notesJSON, err := json.Marshal(notes)
+			if err != nil {
+				log.Printf("Ошибка при сериализации заметок: %v", err)
+			} else {
+				err := database.RedisClient.Set(fmt.Sprintf("notes/%d", authorId), string(notesJSON), 1440*time.Minute).Err()
+				if err != nil {
+					log.Printf("Ошибка при сохранении в Redis: %v", err)
+				}
+			}
+			ctx.JSON(http.StatusOK, notes)
+		}
+	} else {
+		log.Printf("Кеш найден, загружаем из кеша")
+		notes := make([]models.Note, 0)
+		json.Unmarshal([]byte(val), &notes)
+		ctx.JSON(http.StatusOK, notes)
+	}
+
+}
+func resetCache(val string) {
+	database.RedisClient.Del(val)
 }
 
+func recordCasheToRedis(notes []models.Note, authorId int) {
+	notesJSON, err := json.Marshal(notes)
+
+	if err != nil {
+		log.Printf("Ошибка при сериализации заметок: %v", err)
+	} else {
+		err := database.RedisClient.Set(fmt.Sprintf("notes/%d", authorId), string(notesJSON), 1440*time.Minute).Err()
+		if err != nil {
+			log.Printf("Ошибка при сохранении в Redis: %v", err)
+		}
+	}
+}
+func getFromCache(val string, ctx *gin.Context) {
+	log.Printf("Кеш найден, загружаем из Кеша")
+	notes := make([]models.Note, 0)
+	json.Unmarshal([]byte(val), &notes)
+	ctx.JSON(http.StatusOK, notes)
+}
 func DeleteNoteHandler(ctx *gin.Context) {
 	//ctx.JSON(http.StatusOK, "DeleteNoteHandler")
+	var authorID = 1
 	id := ctx.Param("id")
 
 	collection := database.MongoClient.Database("admin").Collection(fmt.Sprintf("notes/%d", 1))
@@ -36,6 +130,7 @@ func DeleteNoteHandler(ctx *gin.Context) {
 	if result.DeletedCount == 0 {
 		ctx.JSON(http.StatusNotFound, "Заметка не найдена")
 	} else {
+		resetCache(fmt.Sprintf("notes/%d", authorID))
 		ctx.JSON(http.StatusNotFound, "Заметка успешно удалена")
 	}
 }
@@ -74,6 +169,7 @@ func UpdateNoteHandler(ctx *gin.Context) {
 	if result.MatchedCount == 0 {
 		ctx.JSON(http.StatusNotFound, "Заметка не найдена")
 	} else {
+		resetCache(fmt.Sprintf("notes/%d", note.AuthorID))
 		ctx.JSON(http.StatusOK, "Заметка успешно обновлена")
 	}
 }
@@ -98,6 +194,7 @@ func CreateNoteHandler(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": errInsert.Error()})
 	}
 	// Если ошибок нет, то возвращаем заметку и статус 200
+	resetCache(fmt.Sprintf("notes/%d", note.AuthorID))
 	ctx.JSON(http.StatusOK, gin.H{
 		"note":    note,
 		"message": "Заметка успешно создана"})
