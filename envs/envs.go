6fc22f97cb69b66485da5ff97a8f6c23e0045d93
Алексей Захаров <127926741+Frown1ngg@users.noreply.github.com>
2025-07-01 18:10:15 +0700

    upd redis

--- envs/envs.go
+++ envs/envs.go
@@ -1,8 +1,9 @@
 package envs
 
 import (
-	"github.com/joho/godotenv"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var ServerEnvs envs
@@ -13,16 +14,22 @@ type envs struct {
 	MONGO_INITDB_PORT          string
 	MONGO_INITDB_HOST          string
 	NOTES_PORT                 string
+	REDIS_PORT                 string
+	REDIS_HOST                 string
 }
 
 func LoadEnvs() error {
 	if err := godotenv.Load(); err != nil {
 		return err
 	}
+	//MongoDB
 	ServerEnvs.NOTES_PORT = os.Getenv("NOTES_PORT")
 	ServerEnvs.MONGO_INITDB_HOST = os.Getenv("MONGO_INITDB_HOST")
 	ServerEnvs.MONGO_INITDB_PORT = os.Getenv("MONGO_INITDB_PORT")
 	ServerEnvs.MONGO_INITDB_ROOT_USERNAME = os.Getenv("MONGO_INITDB_ROOT_USERNAME")
 	ServerEnvs.MONGO_INITDB_ROOT_PASSWORD = os.Getenv("MONGO_INITDB_ROOT_PASSWORD")
+	//redis
+	ServerEnvs.REDIS_PORT = os.Getenv("REDIS_PORT")
+	ServerEnvs.REDIS_HOST = os.Getenv("REDIS_HOST")
 	return nil
 }
